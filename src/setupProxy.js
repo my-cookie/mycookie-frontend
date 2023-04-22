8db47a05da3a8b5f0e352c73e6108516498a59a9
misung Kim <mizzkkim7@gmail.com>
2023-04-22 12:16:44 +0900

    build 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (app) => {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "http://118.67.130.33:8000",
-      changeOrigin: true
-    })
-  );
-};
+// module.exports = (app) => {
+//   app.use(
+//     "/api",
+//     createProxyMiddleware({
+//       target: "http://118.67.130.33:8000",
+//       changeOrigin: true
+//     })
+//   );
+// };
