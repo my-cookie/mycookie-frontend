6aeb3c9458b8a60aeb7c1bd9cd7616cd87ba5cc3
misung Kim <mizzkkim7@gmail.com>
2023-04-24 15:32:24 +0900

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Router from "./pages/Router";
 import { Reset } from "styled-reset";
 import GlobalFonts from "../src/styles/font";
-import RouteChangeTracker from "./components/RouteChangeTracker";
+import ReactGA from "react-ga";
 
 function App() {
-  RouteChangeTracker();
-
+  const gaTrackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; // 환경 변수에 저장된 추적ID 가져오기
+  ReactGA.initialize(gaTrackingId, { debug: true }); // react-ga 초기화 및 debug 사용
+  ReactGA.pageview(window.location.pathname); // 추적하려는 page 설정
   return (
     <>
       <Reset />
