bbc3ad9254a34fe09de80ba4792fd2dcdaf8ef63
MiniMini-On <parkcy92@gmail.com>
2023-04-26 03:10:32 +0900

    :zap: 쪽지 글자수 제한 추가
    쪽지 글자수 제한 추가

--- src/pages/message/SendMessage.jsx
+++ src/pages/message/SendMessage.jsx
@@ -10,18 +10,23 @@ function SendMessage() {
   const [content, setContent] = useRecoilState(contentAtom);
   const [is_anonymous, setIs_anonymous] = useRecoilState(anonymousAtom);
   const navigate = useNavigate();
+  const emoji1 =
+    /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
 
-  console.log(senderName);
   const checkHandler = () => {
     setIs_anonymous(!is_anonymous);
   };
 
   const handleSetValue = (e) => {
-    setContent(e.target.value);
+    setContent(e.target.value.replace(emoji1, ""));
   };
 
   const cookieSend = () => {
-    navigate("/friendselect");
+    if (content.length === 0) {
+      alert("정성을 1g 더 넣어볼까 ?! 😉");
+    } else {
+      navigate("/friendselect");
+    }
   };
 
   return (
@@ -38,7 +43,7 @@ function SendMessage() {
               에게
             </ToBox>
             <TextBox>
-              <ReadMessageText placeholder="친구에게 보낼 쿠키를 작성해봐!" onChange={(e) => handleSetValue(e)}></ReadMessageText>
+              <ReadMessageText placeholder="친구에게 보낼 쿠키를 작성해봐!" onChange={(e) => handleSetValue(e)} value={content}></ReadMessageText>
             </TextBox>
             <FromBox>{is_anonymous == true ? <p>익명 보냄</p> : <FromRead>{senderName} 보냄</FromRead>}</FromBox>
           </div>
